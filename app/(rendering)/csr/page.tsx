4c8e388435e6cfc9580cd65faa72ad49e0b41725
이기봉 <dhkehd2@gmail.com>
2024-12-01 18:41:34 +0900

    📦 json-server@0.17.4 setup

--- app/(rendering)/csr/page.tsx
+++ app/(rendering)/csr/page.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
-  return <div>CSR</div>;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/posts/1")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (!data) return <p>No Data</p>;
+
+  return (
+    <div>
+      <h1>Client-Side Rendering (CSR)</h1>
+      <p>
+        <strong>Title:</strong> {data.title}
+      </p>
+      <p>
+        <strong>Body:</strong> {data.body}
+      </p>
+    </div>
+  );
 };
 
 export default page;
