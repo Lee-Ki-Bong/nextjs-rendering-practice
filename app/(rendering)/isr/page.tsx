909d5428a6a727a15514f8286f5acf5d905a2158
이기봉 <dhkehd2@gmail.com>
2024-12-01 23:15:04 +0900

    ✨ ISR

--- app/(rendering)/isr/page.tsx
+++ app/(rendering)/isr/page.tsx
@@ -1,7 +1,30 @@
+import PostComponent from "@/components/PostComponent";
+import { Post } from "@/types/posts";
 import React from "react";
 
-const Page = () => {
-  return <div>ISR</div>;
+// 요청이 들어올 때, 최대 60초 동안 캐시된 페이지를 갱신합니다.
+export const revalidate = 20;
+
+const fetchPost = async (): Promise<Post> => {
+  const response = await fetch("http://localhost:4000/posts/3");
+
+  if (!response.ok)
+    throw new Error(
+      `Failed to fetch data: ${response.status} ${response.statusText}`,
+    );
+
+  const data: Post = await response.json();
+  return data;
+};
+
+const Page = async () => {
+  const data = await fetchPost();
+
+  return (
+    <>
+      <PostComponent post={data} />
+    </>
+  );
 };
 
 export default Page;
